f23ab8aae8e99f32c943b924b3075cc038001663
Ênio <146883699+Enio-Antonio@users.noreply.github.com>
2026-05-13 09:24:45 -0300

    Update urls.py

--- backend/usuarios/urls.py
+++ backend/usuarios/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from .views import UsuarioDetail, UsuarioList
+from .views import UsuarioDetail, UsuarioList, ExercicioList, ExercicioDetail, RotinaList, RotinaDetail, RotinaExercicioList, RotinaExercicioDetail
 
 urlpatterns = [
     path("<int:pk>/", UsuarioDetail.as_view(), name="usuarios_detail"),
     path("", UsuarioList.as_view(), name="usuarios_list"),
+    path("rotinas/", RotinaList.as_view(), name="rotinas_list"),
+    path("rotinas/<int:pk>/", RotinaDetail.as_view(), name="rotinas_details"),
+    path("exercicios/", ExercicioList.as_view(), name="exercicios_list"),
+    path("exercicios/<int:pk>/", ExercicioDetail.as_view(), name="exercicios_detail"),
+    path("rotinasexercicios/", RotinaExercicioList.as_view(), name="rotinasexercicios_list"),
+    path("rotinasexercicios/<int:pk>/", RotinaExercicioDetail.as_view(), name="rotinasexercicios_detail")
 ]
