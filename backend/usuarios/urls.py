b5a2f957853f2df73fe44cfe4f0210ecfbd2cd6f
Enio-Antonio <enioaclizieri@gmail.com>
2026-05-19 20:39:38 -0300

    correção das rotas para a lista de exercícios

--- backend/usuarios/urls.py
+++ backend/usuarios/urls.py
@@ -1,14 +1,31 @@
 from django.urls import path
 
-from .views import UsuarioDetail, UsuarioList, ExercicioList, ExercicioDetail, RotinaList, RotinaDetail, RotinaExercicioDetail
+from .views import (
+    ExercicioDetail,
+    ExercicioList,
+    RotinaDetail,
+    RotinaExercicioDetail,
+    RotinaExercicioList,
+    RotinaList,
+    UsuarioDetail,
+    UsuarioList,
+)
 
 urlpatterns = [
     path("<int:pk>/", UsuarioDetail.as_view(), name="usuarios_detail"),
     path("", UsuarioList.as_view(), name="usuarios_list"),
     path("rotinas/", RotinaList.as_view(), name="rotinas_list"),
     path("rotinas/<int:pk>/", RotinaDetail.as_view(), name="rotinas_details"),
+    path(
+        "rotinas/<int:rotina_id>/exercicios/",
+        RotinaExercicioList.as_view(),
+        name="rotinasexercicio_list",
+    ),
     path("exercicios/", ExercicioList.as_view(), name="exercicios_list"),
     path("exercicios/<int:pk>/", ExercicioDetail.as_view(), name="exercicios_detail"),
-    path("rotinasexercicios/", RotinaExercicioList.as_view(), name="rotinasexercicios_list"),
-    path("rotinasexercicios/<str:fk>/", RotinaExercicioDetail.as_view(), name="rotinasexercicios_detail")
+    path(
+        "rotinaexercicios/<int:pk>/",
+        RotinaExercicioDetail.as_view(),
+        name="rotinaexercicio_detail",
+    ),
 ]
--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -43,17 +43,18 @@ class RotinaExercicioDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = RotinaExercicio.objects.all()  # type: ignore
     serializer_class = RotinaExercicioSerializer
 
+
 class RotinaExercicioList(generics.ListCreateAPIView):
     serializer_class = RotinaExercicioSerializer
 
     def get_queryset(self):
         # Pega o rotina_id que veio da URL
-        rotina_id = self.kwargs['rotina_id']
+        rotina_id = self.kwargs["rotina_id"]
         # Filtra a tabela onde a chave estrangeira seja igual ao ID da URL
-        return RotinaExercicio.objects.filter(rotina_id=rotina_id)
+        return RotinaExercicio.objects.filter(rotina_id=rotina_id)  # type: ignore
 
     def perform_create(self, serializer):
         # Pega o ID da rotina diretamente da URL
-        rotina_id = self.kwargs.get('rotina_id')
+        rotina_id = self.kwargs.get("rotina_id")
         # Salva o novo registro forçando a rotina_id da URL
         serializer.save(rotina_id=rotina_id)
