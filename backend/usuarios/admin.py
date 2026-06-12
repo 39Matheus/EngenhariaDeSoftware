7670e69ad3a4d7da2775e8b80df34ee6e2abd37c
Enio-Antonio <enioaclizieri@gmail.com>
2026-06-11 22:38:43 -0300

    fix: ignora as pastas __pycache__

--- backend/usuarios/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Exercicio, Rotina, RotinaExercicio, Usuario
-
-# Register your models here.
-admin.site.register(Usuario)
-admin.site.register(Exercicio)
-admin.site.register(Rotina)
-admin.site.register(RotinaExercicio)
