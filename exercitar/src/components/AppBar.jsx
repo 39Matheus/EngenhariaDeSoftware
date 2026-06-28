c9b80c2768179ac95dbbb442ac541064e32d860e
Enio-Antonio <enioaclizieri@gmail.com>
2026-06-28 19:56:05 -0300

    Remove o avatar da navbar

--- exercitar/src/components/AppBar.jsx
+++ exercitar/src/components/AppBar.jsx
@@ -26,7 +26,7 @@ export default function AppBarComponent() {
                 <Typography
                     variant="h6"
                     noWrap
-                    component="a"
+                    component="p"
                     href="#app-bar-with-responsive-menu"
             sx={{
               mr: 2,
@@ -44,7 +44,6 @@ export default function AppBarComponent() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
